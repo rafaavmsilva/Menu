882bd9d7516a220af825613bf45cc00a456a669a
rafaavmsilva <rafaavmsilva@gmail.com>
2024-12-06 18:11:31 -0300

    Remove global app instance to use only factory pattern

--- app.py
+++ app.py
@@ -57,8 +57,7 @@ def create_app():
     
     return app
 
-app = create_app()
-
 if __name__ == '__main__':
+    app = create_app()
     port = int(os.getenv('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
